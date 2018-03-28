a9d3fd8a2ac13b71b2c7885d9d5220d09c331895
Tom <tom.gibbs@hotmail.co.uk>
2018-03-28 04:59:35 +0100

    UK to USA, Atlantic Crossing functionality complete
    Changed script to entirely switch, rather than incrementing array index in update. This solved random particle effect issue.
    
    3D text added. Position/rotation correction added.
    Particle effects added.

--- AR_Vuforia_EmergingTech/R34AR_prototype_V2/Assets/_TOM/scripts/OceanMarkers.cs
+++ AR_Vuforia_EmergingTech/R34AR_prototype_V2/Assets/_TOM/scripts/OceanMarkers.cs
@@ -6,111 +6,151 @@ using UnityEngine.UI;
 public class OceanMarkers : MonoBehaviour
 {
     [Header("Hierarchy Objects")]
-    //public GUIText Title;
-    //public GUIText Time;
-    //public GUIText Desctiption;
-    public GameObject Empty;
-    public TextMesh Title;
-    public TextMesh Time;
-    public TextMesh Desctiption;
+    public TextMesh Header;
+    public TextMesh Description;
+
 
     [Header("Marker Management")]
     public GameObject[] markersArray;
-    public int markersIndex = 1;
+    public int markersIndex = 0;
 
     private GameObject currentMarker;
     private GameObject nextMarker;
+    private GameObject prevMarker;
 
     private ParticleSystem PS;
+    private ParticleSystem nextPS;
+    private ParticleSystem prevPS;
 
 
     //public bool UKtoUS;
 
-
+    
     void Start ()
     {
-        Title = Empty.GetComponent<TextMesh>();
-        Time = Empty.GetComponent<TextMesh>();
-        Desctiption = Empty.GetComponent<TextMesh>();
-
-
-
         currentMarker = markersArray[markersIndex];
-        nextMarker = markersArray[markersIndex+1];
+        //nextMarker = markersArray[markersIndex+1];
 	}
 
 	void Update ()
     {
         SwitchMarker();
 
-        PS = currentMarker.GetComponent<ParticleSystem>();
-        PS.Play();
-
-        Debug.Log("THE MARKERS INDEX IS: " + markersIndex);
-	}
+        //currentMarker = markersArray[markersIndex];
+        //PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+        //PS.Play();
+    }
 
     void SwitchMarker()
     {
         switch (markersIndex)
         {
             case 1:
-                Debug.Log("CASE 1");
-                Title.text = "Title 1";
-                Time.text = "Time 1";
-                Desctiption.text = "Description 1";
+                currentMarker = markersArray[0];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 1";
+                Description.text = "Description 1";
                 break;
             case 2:
-                Debug.Log("CASE 2");
-                Title.text = "Title 2";
-                Time.text = "Time 2";
-                Desctiption.text = "Description 2";
+                currentMarker = markersArray[1];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 2";
+                Description.text = "Description 2";
+                prevMarker = markersArray[0];
+                prevPS = prevMarker.GetComponentInChildren<ParticleSystem>();
+                prevPS.Stop();
                 break;
             case 3:
-                Debug.Log("CASE 3");
-                Title.text = "Title 3";
-                Time.text = "Time 3";
-                Desctiption.text = "Description 3";
+                currentMarker = markersArray[2];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 3";
+                Description.text = "Description 3";
+                prevMarker = markersArray[1];
+                prevPS = prevMarker.GetComponentInChildren<ParticleSystem>();
+                prevPS.Stop();
                 break;
             case 4:
-                Debug.Log("CASE 4");
-                Title.text = "Title 4";
-                Time.text = "Time 4";
-                Desctiption.text = "Description 4";
+                currentMarker = markersArray[3];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 4";
+                Description.text = "Description 4";
+                prevMarker = markersArray[2];
+                prevPS = prevMarker.GetComponentInChildren<ParticleSystem>();
+                prevPS.Stop();
                 break;
             case 5:
-                Title.text = "Title 5";
-                Time.text = "Time 5";
-                Desctiption.text = "Description 5";
+                currentMarker = markersArray[4];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 5";
+                Description.text = "Description 5";
+                prevMarker = markersArray[3];
+                prevPS = prevMarker.GetComponentInChildren<ParticleSystem>();
+                prevPS.Stop();
                 break;
             case 6:
-                Title.text = "Title 6";
-                Time.text = "Time 6";
-                Desctiption.text = "Description 6";
+                currentMarker = markersArray[5];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 6";
+                Description.text = "Description 6";
+                prevMarker = markersArray[4];
+                prevPS = prevMarker.GetComponentInChildren<ParticleSystem>();
+                prevPS.Stop();
                 break;
             case 7:
-                Title.text = "Title 7";
-                Time.text = "Time 7";
-                Desctiption.text = "Description 7";
+                currentMarker = markersArray[6];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 7";
+                Description.text = "Description 7";
+                prevMarker = markersArray[5];
+                prevPS = prevMarker.GetComponentInChildren<ParticleSystem>();
+                prevPS.Stop();
                 break;
             case 8:
-                Title.text = "Title 8";
-                Time.text = "Time 8";
-                Desctiption.text = "Description 8";
+                currentMarker = markersArray[7];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 8";
+                Description.text = "Description 8";
+                prevMarker = markersArray[6];
+                prevPS = prevMarker.GetComponentInChildren<ParticleSystem>();
+                prevPS.Stop();
                 break;
             case 9:
-                Title.text = "Title 9";
-                Time.text = "Time 9";
-                Desctiption.text = "Description 9";
+                currentMarker = markersArray[8];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 9";
+                Description.text = "Description 9";
+                prevMarker = markersArray[7];
+                prevPS = prevMarker.GetComponentInChildren<ParticleSystem>();
+                prevPS.Stop();
                 break;
             case 10:
-                Title.text = "Title 10";
-                Time.text = "Time 10";
-                Desctiption.text = "Description 10";
+                currentMarker = markersArray[9];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 10";
+                Description.text = "Description 10";
+                prevMarker = markersArray[8];
+                prevPS = prevMarker.GetComponentInChildren<ParticleSystem>();
+                prevPS.Stop();
                 break;
             case 11:
-                Title.text = "Title 11";
-                Time.text = "Time 11";
-                Desctiption.text = "Description 11";
+                currentMarker = markersArray[10];
+                PS = currentMarker.GetComponentInChildren<ParticleSystem>();
+                PS.Play();
+                Header.text = "Title 11";
+                Description.text = "Description 11";
+                prevMarker = markersArray[9];
+                prevPS = prevMarker.GetComponentInChildren<ParticleSystem>();
+                prevPS.Stop();
                 break;
         }
     }   
