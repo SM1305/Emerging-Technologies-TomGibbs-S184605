5bc6e1ee70aa23f6aa662a8f9cbd08797064d5e1
Tom <tom.gibbs@hotmail.co.uk>
2018-04-10 05:22:43 +0100

    Material colour change, rather than shader border
    Shader border is either a poor choice for the body of the R34 or the Gondolas.
    
    To preserve as much visual detail of the model as possible, basic colour chnage will be used to highlight selected object.

---
+++ AR_Vuforia_EmergingTech/R34AR_prototype_V2/Assets/_Tom_S184605/scripts/materialtest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class materialtest : MonoBehaviour
+{
+    public Renderer rend;
+    Color normalColour;
+    Color highlight = new Color(0, 1, 0);
+    
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        normalColour = rend.material.color;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rend.material.color = highlight;
+            Debug.Log("highlight colour");
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            rend.material.color = normalColour;
+            Debug.Log("normal colour");
+        }
+    }
+}
---
+++ AR_Vuforia_EmergingTech/R34AR_prototype_V2/Assets/_Tom_S184605/scripts/shadertest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shadertest : MonoBehaviour
+{
+    Shader shader1;
+    Shader shader2;
+    public Renderer rend;
+
+
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();
+        shader1 = Shader.Find("Standard");
+        shader2 = Shader.Find("N3K/Outline");
+        rend.material.shader = shader1;
+    }
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rend.material.shader = shader2;
+            Debug.Log("shader 2");
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            rend.material.shader = shader1;
+            Debug.Log("shader 1");
+        }
+    }
+}
