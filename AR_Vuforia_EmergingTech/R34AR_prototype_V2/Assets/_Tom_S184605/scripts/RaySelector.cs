f49b4247957d9a8c54d21ae0bc62bbaee003d1f1
Tom <tom.gibbs@hotmail.co.uk>
2018-04-29 22:14:00 +0100

    Working on inclusion of cardboard
    .

--- AR_Vuforia_EmergingTech/R34AR_prototype_V2/Assets/_Tom_S184605/scripts/RaySelector.cs
+++ AR_Vuforia_EmergingTech/R34AR_prototype_V2/Assets/_Tom_S184605/scripts/RaySelector.cs
@@ -4,22 +4,39 @@ using UnityEngine;
 using Vuforia;
 using UnityEngine.UI;
 
-public class RaySelector : MonoBehaviour, IVirtualButtonEventHandler
+public class RaySelector : MonoBehaviour, IVirtualButtonEventHandler //inherits from IVirtualButtonEventHandler to provide virtual button detection
 {
+    //virtual buttons
     public GameObject vButtonSelect;
     public GameObject vButtonSpawn;
 
-    public enum SelectableObjects { Engines, ControlEngineCar, WingEngineCar, RearEngineCar, R34, Wheel };
+    //vaariables for moving ARcam
+    //public Camera ARcam;
+    public GameObject Cam;
+    public GameObject[] markerArray;
+    public GameObject focalPointCockpit, focalPointMid, focalPointStern;
+    public float speed = 0.1f;
+    Vector3 currentPos;
+    Vector3 endPos;
+    
+    //application states
+    public enum SelectableObjects { Engine, ControlEngineCar, WingEngineCar, RearEngineCar, R34, Wheel,
+                                    Marker1, Marker2, Marker3, Marker4, Marker5, Marker6, Marker7, Marker8, Marker9, Marker10, Marker11, Marker12,
+                                    Blank };
     public SelectableObjects objects;
-
+    
+    //raycast for object selection
     public RaycastHit hitInfo;
     LineRenderer rayLine;
     public Material LRmat1, LRmat2;
     public AudioSource rayAudio;
 
+    //used to display information
     public Text header, body;
 
     //DeployStageOnce _deployStageOnce;
+
+
 
 
     void Start ()
@@ -36,29 +53,35 @@ public class RaySelector : MonoBehaviour, IVirtualButtonEventHandler
         header.enabled = true;
         body.text = "";
         body.enabled = true;
+
+        objects = SelectableObjects.Blank; //clears any UI text from being displayed on start
     }
 
     void Update()
     {
         AimLine();
-
-        //SearchObjects();
-
         SelectedObjects();
+
+
+        //if (ARcam.transform.position == endPos) //removes UI text "moving" when ARcam has reached its destination
+        //{
+        //    header.text = "";
+        //    body.text = "";
+        //}
     }
 
 
-
+    //Called when virtual button is blocked from device view by user interaction 
     public void OnButtonPressed(VirtualButtonBehaviour vButton)
     {
         if (vButton.gameObject.name == "VirtualButton_select")
         {
             Debug.Log("SELECT BUTTON PRESSED");
 
-            rayLine.material = LRmat2;
-            rayLine.SetWidth(1, 1);
+            rayLine.material = LRmat2; //changes appearance of line renderer for duration of button press
+            rayLine.SetWidth(1, 1); //increases line renderer width for duration of button press
 
-            if (Physics.Raycast(transform.position, transform.forward, out hitInfo))
+            if (Physics.Raycast(transform.position, transform.forward, out hitInfo)) //gets information from object hit by raycast
             {
                 Debug.Log(hitInfo.transform.name);
 
@@ -66,7 +89,7 @@ public class RaySelector : MonoBehaviour, IVirtualButtonEventHandler
                     rayLine.SetPosition(1, hitInfo.point);
             }
 
-            SearchObjects();
+            SearchObjects(); //function compares name of object hit by raycast to strings, to select next application state
         }
         if (vButton.gameObject.name == "VirtualButton_spawn")
         {
@@ -75,16 +98,15 @@ public class RaySelector : MonoBehaviour, IVirtualButtonEventHandler
         }
     }
 
-
-
+    //called when blocked virtual button is recognised by device 
     public void OnButtonReleased(VirtualButtonBehaviour vButton)
     {
         if (vButton.gameObject.name == "VirtualButton_select")
         {
             Debug.Log("Select button RELEASED");
 
-            rayLine.material = LRmat1;
-            rayLine.SetWidth(0.2f, 0.2f);
+            rayLine.material = LRmat1; //sets linerenderer material to default
+            rayLine.SetWidth(0.2f, 0.2f); //sets linerenderer width to default
         }
         if (vButton.gameObject.name == "VirtualButton_spawn")
         {
@@ -94,7 +116,7 @@ public class RaySelector : MonoBehaviour, IVirtualButtonEventHandler
     }
 
 
-
+    //makes raycast visible in application. Allows user to see where Vuforia controller is aiming before pressing virtual button.
     void AimLine()
     {
         rayLine.SetPosition(0, transform.position);
@@ -105,18 +127,20 @@ public class RaySelector : MonoBehaviour, IVirtualButtonEventHandler
     }
 
 
-
+    //compares object hit by raycast to strings
+    //result decides the next application enum state
     void SearchObjects()
     {
+        //airship colliders
         if (hitInfo.transform.name == "R34")
         {
             Debug.Log("R34");
             objects = SelectableObjects.R34;
         }
-        if (hitInfo.transform.name == "Engines")
+        if (hitInfo.transform.name == "Engine")
         {
-            Debug.Log("Engines");
-            objects = SelectableObjects.Engines;
+            Debug.Log("Engine");
+            objects = SelectableObjects.Engine;
         }
         if (hitInfo.transform.name == "ControlEngineCar")
         {
@@ -138,52 +162,255 @@ public class RaySelector : MonoBehaviour, IVirtualButtonEventHandler
             Debug.Log("Wheel");
             objects = SelectableObjects.Wheel;
         }
+
+        //movement marker colliders
+        if (hitInfo.transform.name == "Marker1")
+        {
+            Debug.Log("Marker1");
+            objects = SelectableObjects.Marker1;
+        }
+        if (hitInfo.transform.name == "Marker2")
+        {
+            Debug.Log("Marker2");
+            objects = SelectableObjects.Marker2;
+        }
+        if (hitInfo.transform.name == "Marker3")
+        {
+            Debug.Log("Marker3");
+            objects = SelectableObjects.Marker3;
+        }
+        if (hitInfo.transform.name == "Marker4")
+        {
+            Debug.Log("Marker4");
+            objects = SelectableObjects.Marker4;
+        }
+        if (hitInfo.transform.name == "Marker5")
+        {
+            Debug.Log("Marker5");
+            objects = SelectableObjects.Marker5;
+        }
+        if (hitInfo.transform.name == "Marker6")
+        {
+            Debug.Log("Marker6");
+            objects = SelectableObjects.Marker6;
+        }
+        if (hitInfo.transform.name == "Marker7")
+        {
+            Debug.Log("Marker7");
+            objects = SelectableObjects.Marker7;
+        }
+        if (hitInfo.transform.name == "Marker8")
+        {
+            Debug.Log("Marker8");
+            objects = SelectableObjects.Marker8;
+        }
+        if (hitInfo.transform.name == "Marker9")
+        {
+            Debug.Log("Marker9");
+            objects = SelectableObjects.Marker9;
+        }
+        if (hitInfo.transform.name == "Marker10")
+        {
+            Debug.Log("Marker10");
+            objects = SelectableObjects.Marker10;
+        }
+        if (hitInfo.transform.name == "Marker11")
+        {
+            Debug.Log("Marker11");
+            objects = SelectableObjects.Marker11;
+        }
+        if (hitInfo.transform.name == "Marker12")
+        {
+            Debug.Log("Marker12");
+            objects = SelectableObjects.Marker12;
+        }
     }
 
-
-
-    void SelectedObjects()
+   //uses enum value to select application state
+   //each case will either display information and play visual effect (if a selectable object), or is used to reposition the ARcam to its position (if a marker).
+   void SelectedObjects()
     {
         switch(objects)
         {
+            //selectable objects:
+            //display information, object selected is highlighted.
             case SelectableObjects.ControlEngineCar:
-                header.text = "ControlControlControlControlControlControlControlControlControl";
-                body.text = "ControlControlControlControlControlControlControlControlControlControl";
+                header.text = "Control and Front Engine Car";
+                body.text = "The front gondola comprised of two compartments separated by a narrow gap, intended to prevent vibrations from affecting the equipment.";
                 Debug.Log("ControlEngineCar");
-                SearchObjects();
                 break;
             case SelectableObjects.WingEngineCar:
-                header.text = "WingWingWingWingWingWingWingWingWingWingWing";
-                body.text = "WingWingWingWingWingWingWingWingWingWingWingWing";
-                Debug.Log("WingEngineCaar");
-                SearchObjects();
+                header.text = "Wing Engine Car";
+                body.text = "Two 'wing' gondolas each housed an engine with a reversing gear - a refinement that enabled the airship to be operated if those in the main control-cabin failed.";
+                Debug.Log("WingEngineCar");
                 break;
             case SelectableObjects.RearEngineCar:
-                header.text = "RearRearRearRearRearRearRearRearRearRearRearRear";
-                body.text = "RearRearRearRearRearRearRearRearRearRear";
+                header.text = "Rear Engine Car";
+                body.text = "The rear car, like the Front, was ringed with a rail to assist handlers and two 'bumping bags' of compressed air were positioned underneath to help cushion landing shocks.";
                 Debug.Log("RearEngineCar");
-                SearchObjects();
                 break;
-            case SelectableObjects.Engines:
-                header.text = "PropellerPropellerPropellerPropellerPropellerPropellerPropellerPropeller";
-                body.text = "PropellerPropellerPropellerPropellerPropellerPropellerPropellerPropellerPropellerPropellerPropeller";
-                Debug.Log("Engines");
-                SearchObjects();
+            case SelectableObjects.Engine:
+                header.text = "Engines";
+                body.text = "Each of the five engines was a Sunbeam 'Maori'. No Rolls Royce engines could be made available as all those produced were now reserved for aeroplane use.";
+                Debug.Log("Engine");
                 break;
             case SelectableObjects.R34:
-                header.text = "R34R34R34R34R34R34R34R34R34R34";
-                body.text = "R34R34R34R34R34R34R34R34R34R34R34";
+                header.text = "Hull";
+                body.text = "Running almost the entire length of the ship was a long keel corridor, consisting of a succession of A-shaped frames.";
                 Debug.Log("R34");
-                SearchObjects();
                 break;
             case SelectableObjects.Wheel:
-                header.text = "WheelWheelWheelWheelWheelWheelWheelWheel";
-                body.text = "WheelWheelWheelWheelWheelWheelWheelWheelWheelWheel";
+                header.text = "Steering";
+                body.text = "Each engine had twelve water- cooled cylinders, which were intended to produce full power at a theoretical 2,100 rpm, although in practice it was rare for 1,600 r pm to be exceeded.";
                 Debug.Log("Wheel");
-                SearchObjects();
                 break;
+
+            //markers:
+            //lerp ARcam towards their transform
+            case SelectableObjects.Marker1:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker1");
+                    //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[0].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointCockpit.transform);
+                break;
+            case SelectableObjects.Marker2:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker2");
+                    //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[1].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointCockpit.transform);
+                break;
+            case SelectableObjects.Marker3:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker3");
+                    //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[2].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointCockpit.transform);
+                break;
+            case SelectableObjects.Marker4:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker4");
+                    //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[3].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointCockpit.transform);
+                break;
+            case SelectableObjects.Marker5:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker5");
+                    //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[4].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointCockpit.transform);
+                break;
+            case SelectableObjects.Marker6:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker6");
+                    //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[5].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointCockpit.transform);
+                break;
+            case SelectableObjects.Marker7:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker7");
+                    //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[6].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointMid.transform);
+                break;
+            case SelectableObjects.Marker8:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker8");
+                    //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[7].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointMid.transform);
+                break;
+            case SelectableObjects.Marker9:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker9");
+                    //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[8].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointMid.transform);
+                break;
+            case SelectableObjects.Marker10:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker10");
+                    //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[9].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointMid.transform);
+                break;
+            case SelectableObjects.Marker11:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker11");
+                //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[10].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointStern.transform);
+                break;
+            case SelectableObjects.Marker12:
+                header.text = "Moving to waypoint";
+                body.text = "";
+                Debug.Log("marker12");
+                //movement
+                currentPos = Cam.transform.position;
+                endPos = markerArray[11].transform.position;
+                endPos.y = -23;
+                Cam.transform.position = Vector3.Lerp(currentPos, endPos, speed);
+                Cam.transform.LookAt(focalPointStern.transform);
+                break;
+
+            //show no UI at start
+            case SelectableObjects.Blank:
+                header.text = "";
+                body.text = "";
+                Debug.Log("Blank");
+                break;
+            //default
             default:
-                Debug.Log("default innit");
+                header.text = "";
+                body.text = "";
+                Debug.Log("defaultcase");
                 break;
         }
     }
---
+++ AR_Vuforia_EmergingTech/R34AR_prototype_V2/Assets/_Tom_S184605/scripts/SelectedObjectEffect.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedObjectEffect : MonoBehaviour
+{
+    RaySelector _raySelector;
+    public GameObject RaySelectorGO;
+
+    public GameObject R34;
+    Renderer R34renderer;
+
+    public GameObject propeller1, propeller2, propeller3, propeller4;
+    Renderer propeller1renderer, propeller2renderer, propeller3renderer, propeller4renderer;
+
+    public GameObject ControlEngineCar;
+    Renderer ControlEngineCarrenderer;
+    Material[] ControlEngineCarmat;
+
+    public GameObject WingEngine1, WingEngine2;
+    Renderer WingEngine1renderer, WingEngine2renderer;
+    Material[] WingEngine1mat, WingEngine2mat;
+
+    public GameObject RearEngine;
+    Renderer RearEnginerenderer;
+    Material[] RearEnginemat;
+
+    public GameObject[] Markers;
+    Renderer markerRenderer1, markerRenderer2, markerRenderer3, markerRenderer4, markerRenderer5, markerRenderer6, markerRenderer7, markerRenderer8, markerRenderer9, markerRenderer10, markerRenderer11, markerRenderer12;
+
+
+    Color normalColour;
+    Color highlight = new Color(1, 1, 0);
+
+    void Start()
+    {
+        _raySelector = RaySelectorGO.GetComponent<RaySelector>();
+
+        R34renderer = R34.GetComponent<Renderer>();
+
+        propeller1renderer = propeller1.GetComponent<Renderer>();
+        propeller2renderer = propeller2.GetComponent<Renderer>();
+        propeller3renderer = propeller3.GetComponent<Renderer>();
+        propeller4renderer = propeller4.GetComponent<Renderer>();
+
+        ControlEngineCarrenderer = ControlEngineCar.GetComponent<Renderer>();
+        ControlEngineCarmat = ControlEngineCarrenderer.materials;
+
+        WingEngine1renderer = WingEngine1.GetComponent<MeshRenderer>();
+        WingEngine1mat = WingEngine1renderer.materials;
+        WingEngine2renderer = WingEngine2.GetComponent<MeshRenderer>();
+        WingEngine2mat = WingEngine2renderer.materials;
+
+        RearEnginerenderer = RearEngine.GetComponent<Renderer>();
+        RearEnginemat = RearEnginerenderer.materials;
+
+        markerRenderer1 = Markers[0].GetComponent<Renderer>();
+        markerRenderer2 = Markers[1].GetComponent<Renderer>();
+        markerRenderer3 = Markers[2].GetComponent<Renderer>();
+        markerRenderer4 = Markers[3].GetComponent<Renderer>();
+        markerRenderer5 = Markers[4].GetComponent<Renderer>();
+        markerRenderer6 = Markers[5].GetComponent<Renderer>();
+        markerRenderer7 = Markers[6].GetComponent<Renderer>();
+        markerRenderer8 = Markers[7].GetComponent<Renderer>();
+        markerRenderer9 = Markers[8].GetComponent<Renderer>();
+        markerRenderer10 = Markers[9].GetComponent<Renderer>();
+        markerRenderer11 = Markers[10].GetComponent<Renderer>();
+        markerRenderer12 = Markers[11].GetComponent<Renderer>();
+
+        normalColour = R34renderer.material.color;
+    }
+
+    void Update()
+    {
+        if (_raySelector.objects == RaySelector.SelectableObjects.R34)
+            R34renderer.material.color = highlight;
+        else
+            R34renderer.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Engine)
+        {
+            propeller1renderer.material.color = highlight;
+            propeller2renderer.material.color = highlight;
+            propeller3renderer.material.color = highlight;
+            propeller4renderer.material.color = highlight;
+
+            propeller1.transform.Rotate(Vector3.right * Time.deltaTime * 50);
+            propeller2.transform.Rotate(Vector3.right * Time.deltaTime * 50);
+            propeller3.transform.Rotate(Vector3.right * Time.deltaTime * 50);
+            propeller4.transform.Rotate(Vector3.right * Time.deltaTime * 50);
+        }
+        else
+        {
+            propeller1renderer.material.color = normalColour;
+            propeller2renderer.material.color = normalColour;
+            propeller3renderer.material.color = normalColour;
+            propeller4renderer.material.color = normalColour;
+
+            propeller1.transform.Rotate(0f, 0f, 0f, Space.Self);
+            propeller2.transform.Rotate(0f, 0f, 0f, Space.Self);
+            propeller3.transform.Rotate(0f, 0f, 0f, Space.Self);
+            propeller4.transform.Rotate(0f, 0f, 0f, Space.Self);
+        }
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.ControlEngineCar)
+        {
+            //ControlEngineCarrenderer.material.color = highlight;
+            ControlEngineCarmat[0].color = highlight;
+            ControlEngineCarmat[1].color = highlight;
+            ControlEngineCarmat[2].color = highlight;
+        }
+        else
+        {
+            //ControlEngineCarrenderer.material.color = normalColour;
+            ControlEngineCarmat[0].color = normalColour;
+            ControlEngineCarmat[1].color = normalColour;
+            ControlEngineCarmat[2].color = normalColour;
+        }
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.WingEngineCar)
+        {
+            //WingEngine1renderer.material.color = highlight;
+            WingEngine1mat[0].color = highlight;
+            WingEngine1mat[1].color = highlight;
+            WingEngine1mat[2].color = highlight;
+            WingEngine1mat[3].color = highlight;
+            //WingEngine2renderer.material.color = highlight;
+            WingEngine2mat[0].color = highlight;
+            WingEngine2mat[1].color = highlight;
+            WingEngine2mat[2].color = highlight;
+            WingEngine2mat[3].color = highlight;
+        }
+        else
+        {
+            //WingEngine1renderer.material.color = normalColour;
+            WingEngine1mat[0].color = normalColour;
+            WingEngine1mat[1].color = normalColour;
+            WingEngine1mat[2].color = normalColour;
+            WingEngine1mat[3].color = normalColour;
+            //WingEngine2renderer.material.color = normalColour;
+            WingEngine2mat[0].color = normalColour;
+            WingEngine2mat[1].color = normalColour;
+            WingEngine2mat[2].color = normalColour;
+            WingEngine2mat[3].color = normalColour;
+        }
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.RearEngineCar)
+        {
+            //RearEnginerenderer.material.color = highlight;
+            RearEnginemat[0].color = highlight;
+            RearEnginemat[1].color = highlight;
+            RearEnginemat[2].color = highlight;
+        }
+        else
+        {
+            //RearEnginerenderer.material.color = normalColour;
+            RearEnginemat[0].color = normalColour;
+            RearEnginemat[1].color = normalColour;
+            RearEnginemat[2].color = normalColour;
+        }
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker1)
+            markerRenderer1.material.color = highlight;
+        else
+            markerRenderer1.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker2)
+            markerRenderer2.material.color = highlight;
+        else
+            markerRenderer2.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker3)
+            markerRenderer3.material.color = highlight;
+        else
+            markerRenderer3.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker4)
+            markerRenderer4.material.color = highlight;
+        else
+            markerRenderer4.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker5)
+            markerRenderer5.material.color = highlight;
+        else
+            markerRenderer5.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker6)
+            markerRenderer6.material.color = highlight;
+        else
+            markerRenderer6.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker7)
+            markerRenderer7.material.color = highlight;
+        else
+            markerRenderer7.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker8)
+            markerRenderer8.material.color = highlight;
+        else
+            markerRenderer8.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker9)
+            markerRenderer9.material.color = highlight;
+        else
+            markerRenderer9.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker10)
+            markerRenderer10.material.color = highlight;
+        else
+            markerRenderer10.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker11)
+            markerRenderer11.material.color = highlight;
+        else
+            markerRenderer11.material.color = normalColour;
+
+        if (_raySelector.objects == RaySelector.SelectableObjects.Marker12)
+            markerRenderer12.material.color = highlight;
+        else
+            markerRenderer12.material.color = normalColour;
+    }
+}
